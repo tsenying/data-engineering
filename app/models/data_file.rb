b7b0e4d14c2cadb73006eb01eced95043a53779e
Ying Tsen Hong <tsenying@gmail.com>
2014-01-26 19:51:49 -0700

    add data file processing

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -1,7 +1,23 @@
 class DataFile < ActiveRecord::Base
-  attr_accessible :file_name, :total
 
   def process(data_file)
-    self.total = 123
+    data_file.readline # skip header line
+
+    self.total = 0
+    data_file.each do |line|
+      logger.info("line:#{line}")
+      fields = line.split(/\t/);
+      logger.info("fields:#{fields.inspect}")
+      self.total += fields[2].to_f * fields[3].to_i
+
+      purchase = Purchase.new(:count => fields[3])
+      purchase.purchaser = Purchaser.where(:name => fields[0]).first_or_create
+      purchase.merchant = Merchant.where(:name => fields[5], :address => fields[4]).first_or_create
+      purchase.item = Item.where(:description => fields[1], :price => fields[2]).first_or_create
+
+      purchase.save
+    end
+
+    logger.info("\n\n!!!!!!!!total=#{total}, self.total=#{self.total}")
   end
 end
