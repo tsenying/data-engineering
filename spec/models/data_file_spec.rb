b6809e7155bc8a499773abbae611d0139a77a15a
Ying Tsen Hong <tsenying@gmail.com>
2014-01-26 19:49:51 -0700

    add spec for data file processing

--- spec/models/data_file_spec.rb
+++ spec/models/data_file_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe DataFile do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should process data file" do
+    data_file = File.open('spec/data/example_input.tab')
+    @data_file = DataFile.new()
+    @data_file.process(data_file)
+    @data_file.total.should == 95.0
+  end
 end
